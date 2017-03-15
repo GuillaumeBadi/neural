c527966529735b01ad83609f7216c8ad7e0883fd
Guillaume <guillaume.badi@gmail.com>
2017-03-15 10:35:25 +0100

    additional data

--- Neural.hs
+++ Neural.hs
@@ -14,6 +14,16 @@ hmult :: Num a => Matrix a -> Matrix a -> Matrix a
 hmult m m' = matrix (nrows m) (ncols m) multE
   where multE (i, j) = unsafeGet i j m * unsafeGet i j m'
 
+madd :: Num a => Matrix a -> Matrix a -> Matrix a
+madd m m' = mmap add m
+  where add i j e = (unsafeGet i j m') + e
+
+smult :: Num a => a -> Matrix a -> Matrix a
+smult = scaleMatrix
+
+mmult :: Num a => Matrix a -> Matrix a -> Matrix a
+mmult = multStd2
+
 mT :: Matrix a -> Matrix a
 mT = transpose
 
